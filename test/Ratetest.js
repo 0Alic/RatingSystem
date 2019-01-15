35225efd9c807fd153403f66af20b9654023eb2b
0Alic <andrealisi.12lj@gmail.com>
2019-01-15 19:09:56 +0100

    Updated test script

--- test/Ratetest.js
+++ test/Ratetest.js
@@ -18,21 +18,12 @@ contract("RatingSystemFramework", accounts => {
     const sender = accounts[0];
     var tx;
 
-    getInstanceAtPosition = async(list, pos, contractType) => {
-
-        return await contractType.at(list[pos]);
-    }
-
     it("Should create a user called " + bobName, async() => {
 
         const ratingSystem = await RatingSystem.deployed();
         tx = await ratingSystem.createUser(web3.utils.fromUtf8(bobName), {from: bob});
-        const bobObject = await getInstanceAtPosition(await ratingSystem.getUsers(), 0, User);
-//        console.log(await ratingSystem.userCount());
-//        const userList = await ratingSystem.getUsers();
-//        const bobObjectAddress = userList[0]; 
-//        const bobObject = await User.at(bobObjectAddress);
-
+        const bobUserAddress = await ratingSystem.getMyUserContract({from: bob});
+        const bobObject = await User.at(bobUserAddress);
 
         // Check ownership
         const owner = await bobObject.owner();
@@ -45,29 +36,24 @@ contract("RatingSystemFramework", accounts => {
 
         // Retrieve bob's User contract instance        
         const ratingSystem = await RatingSystem.deployed();
-        const users = await ratingSystem.getUsers();
-        const bobObject = await getInstanceAtPosition(await ratingSystem.getUsers(), 0, User);
-        // const userList = await ratingSystem.getUsers();
-        // const bobObjectAddress = userList[0]; 
-        // const bobObject = await User.at(bobObjectAddress);
+        const bobUserAddress = await ratingSystem.getMyUserContract({from: bob});
+        const bobObject = await User.at(bobUserAddress);
         // Create Item for bob
         const tx = await bobObject.createItem(web3.utils.fromUtf8(bobItemName), {from: bob});
         // Retrieve item's contract instance
-        const itemObject = await getInstanceAtPosition(await bobObject.getItems(), 0, Item);
-        // const itemList = await bobObject.getItems();
-        // const deployedItemAddress = itemList[0];
-        // const itemObject = await Item.at(deployedItemAddress);
+        const itemList = await bobObject.getItems();
+        const deployedItemAddress = itemList[0];
+        const itemObject = await Item.at(deployedItemAddress);
 
         const itemName = web3.utils.toUtf8(await itemObject.name())
-//        console.log("Item name: " + itemName);     
         assert.equal(itemName, bobItemName, "The item is not " + bobItemName);
     }); 
 
     it("Should remove and insert again " + bobName, async() => {
 
         const ratingSystem = await RatingSystem.deployed();
-        const bobObject = await getInstanceAtPosition(await ratingSystem.getUsers(), 0, User);
-//        const bobObject = await getInstanceAtPosition(await ratingSystem.getUsers(), 0, User);
+        const bobUserAddress = await ratingSystem.getMyUserContract({from: bob});
+        const bobObject = await User.at(bobUserAddress);
         await ratingSystem.deleteUser(bobObject.address, {from: bob});
         await ratingSystem.createUser(web3.utils.fromUtf8(bobName), {from: bob});
         
@@ -77,7 +63,6 @@ contract("RatingSystemFramework", accounts => {
             const userObject = await User.at(user);
             const bytes = await userObject.name();
             const name = web3.utils.toUtf8(bytes);
-            console.log(name);
         }
     });
 
@@ -96,85 +81,4 @@ contract("RatingSystemFramework", accounts => {
         const ratingSystem = await RatingSystem.deployed();
     });
 
-
-    /*
-    const n = 5;
-    it("Should insert " + n + " Items", async() => {
-
-        const ratingSystem = await RatingSystem.deployed();
-        const userList = await ratingSystem.getAssets();
-        const userAddress = userList[0];
-        const userObject = await User.at(userAddress);
-
-        for(let i=0; i<n; i++) {
-
-            let tx = await userObject.createItem(web3.utils.fromUtf8("Item " + 1), {from: user});
-        }
-    })
-
-    it("Should test how long does it take to retrieve " + n + " Items", async() => {
-
-        const ratingSystem = await RatingSystem.deployed();
-        const userList = await ratingSystem.getAssets();
-        const userAddress = userList[0];
-        const userObject = await User.at(userAddress);
-
-        const itemList = await userObject.getAssets();
-        console.log(itemList);
-    })
-
-    const m = 10;
-    it("Should insert " + n + " ratings", async() => {
-
-        const ratingSystem = await RatingSystem.deployed();
-        
-        // Retrieve User
-        const userList = await ratingSystem.getAssets();
-        const userAddress = userList[0];
-        const userObject = await User.at(userAddress);
-
-        // Retrieve Item
-        const itemList = await userObject.getAssets();
-        const deployedItemAddress = itemList[0];
-        const itemObject = await Item.at(deployedItemAddress);
-
-        const min = 1;
-        const max = 10;
-        let scores = [];
-
-        for(let i=0; i<m; i++) {
-
-            let score = Math.floor(Math.random() * (max - min)) + min;
-            scores.push(score);
-            let tx = await itemObject.grantPermission(sender, {from: user});
-            tx = await itemObject.rate(score, Date.now(), {from: sender});
-        }
-    });
-
-    it("Should test how long does it take to compute the mean on " + n + " ratings", async() => {
-
-        const ratingSystem = await RatingSystem.deployed();
-        
-        // Retrieve User
-        const userList = await ratingSystem.getAssets();
-        const userAddress = userList[0];
-        const userObject = await User.at(userAddress);
-
-        // Retrieve Item
-        const itemList = await userObject.getAssets();
-        const deployedItemAddress = itemList[0];
-        const itemObject = await Item.at(deployedItemAddress);
-
-        console.log(await itemObject.computeRate());
-
-        const dataBundle = await itemObject.getAllRatings();
-        const scores = dataBundle._scores;
-        const timestamps = dataBundle._timestamps;
-        const raters = dataBundle._raters;
-
-        console.log(scores.map( e => e.toNumber()));
-        console.log(timestamps.map( e => e.toNumber()));
-        console.log(raters);
-    });
-    */
 });
