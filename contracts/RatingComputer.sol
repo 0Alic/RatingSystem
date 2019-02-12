7117856d282e001337909051ee0d5457aeeb1bc9
0Alic <andrealisi.12lj@gmail.com>
2019-02-12 16:28:25 +0100

    removed address[] from RatingComputer.compute()

--- contracts/RatingComputer.sol
+++ contracts/RatingComputer.sol
@@ -1,19 +1,95 @@
 pragma solidity ^0.5.0;
 
+// /// @title RatingComputer
+// /// @notice This contract interface defines the method to compute the final score of a list of scores
+// interface RatingComputer {
+
+//     modifier haveEqualLength(uint[] memory _s, uint[] memory _b, address[] memory _r) {
+
+//         require(_s.length == _b.length && _b.length == _r.length);
+//         _;
+//     }
+
+//     /// @notice Compute the final score given a bundle of rating information
+//     /// @param _scores The array of scores (1 to 10)
+//     /// @param _blocks The array of blocks containing the scores
+//     /// @param _raters The raters connected to the scores
+//     function compute(uint[] calldata _scores, 
+//                      uint[] calldata _blocks, 
+//                      address[] calldata _raters) external pure returns(uint);
+// }
+
+
+// /// @title SimpleAverageComputer
+// /// @notice Compute the final score of a list of scores using the simple average formula
+// contract SimpleAvarageComputer is RatingComputer {
+    
+//     function compute(uint[] calldata _scores, 
+//                      uint[] calldata _blocks, 
+//                      address[] calldata _raters) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
+
+//         uint len = _scores.length;
+
+//         if (len <= 0) 
+//             return 0;
+ 
+//         // Simple average
+//         uint total = 0;
+
+//         for (uint i=0; i<len; i++)
+//             total += _scores[i];
+
+//         return total / len;
+//     }
+// }
+
+
+// /// @title WeightedAverageComputer
+// /// @notice Compute the final score of a list of scores using a weighted average formula, assigning less weight to newer scores
+// contract WeightedAverageComputer is RatingComputer {
+
+//     function compute(uint[] calldata _scores, 
+//                      uint[] calldata _blocks, 
+//                      address[] calldata _raters) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
+
+//         uint len = _scores.length;
+
+//         if (len <= 0) 
+//             return 0;
+ 
+//         // Weighted average w.r.t. blocks
+//         uint last = _blocks[len-1];
+//         uint total = 0;
+//         uint weightSum = 0;
+
+//         for (uint i=0; i<len; i++) {
+
+//             uint s = _scores[i];
+//             uint b = _blocks[i];
+//             uint weight = (b*100)/last;
+
+//             total += s * weight;
+//             weightSum += weight;
+//         }
+
+//         return total / weightSum;
+//     }
+// }
+
+
 /// @title RatingComputer
 /// @notice This contract interface defines the method to compute the final score of a list of scores
 interface RatingComputer {
 
-    modifier haveEqualLength(uint[] memory _s, uint[] memory _b, address[] memory _r) {
+    modifier haveEqualLength(uint[] memory _s, uint[] memory _b) {
 
-        require(_s.length == _b.length && _b.length == _r.length);
+        require(_s.length == _b.length);
         _;
     }
 
     /// @notice Compute the final score given a bundle of rating information
     /// @param _scores The array of scores (1 to 10)
     /// @param _blocks The array of blocks containing the scores
-    /// @param _raters The raters connected to the scores
     function compute(uint[] calldata _scores, 
                      uint[] calldata _blocks, 
                      address[] calldata _raters) external pure returns(uint);
@@ -25,8 +101,7 @@ interface RatingComputer {
 contract SimpleAvarageComputer is RatingComputer {
     
     function compute(uint[] calldata _scores, 
-                     uint[] calldata _blocks, 
-                     address[] calldata _raters) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
+                     uint[] calldata _blocks) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
 
         uint len = _scores.length;
 
@@ -49,8 +124,7 @@ contract SimpleAvarageComputer is RatingComputer {
 contract WeightedAverageComputer is RatingComputer {
 
     function compute(uint[] calldata _scores, 
-                     uint[] calldata _blocks, 
-                     address[] calldata _raters) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
+                     uint[] calldata _blocks) haveEqualLength(_scores, _blocks, _raters) external pure returns(uint) {
 
         uint len = _scores.length;
 
---
+++ interactions/ropsten/testCompute.js
@@ -0,0 +1,131 @@
+const Framework = artifacts.require("./RatingSystemFramework");
+const Storage = artifacts.require("./OwnableCRUDStorage");
+const ComputerRegistry = artifacts.require("./ComputerRegistry");
+const SimpleComputer = artifacts.require("./SimpleAvarageComputer");
+const Item = artifacts.require("./Item");
+
+const fs = require("fs");
+
+var config = JSON.parse(fs.readFileSync('../config.json', 'utf8'));
+//var results = JSON.parse(fs.readFileSync(output, 'utf8'));
+
+const samples = config["ropsten"]["samples"];               // Numer of times I want to call the compute() function
+const elements = config["ropsten"]["ratingList"];           // The list of the dimension of the input arrays 
+const degrees = config["ropsten"]["degrees"];                 // The list of the number of function calls to propagate simultaneously
+const computerObject = config["ropsten"]["ratingComputer"];     // Id of the interested RatingComputer
+
+/**
+ * This script measures the time it takes to executute computeRate() depending on the current number of ratings on an item
+ * 
+ * At the end, we write an entry on the json file results.json with key the current number of ratings and as value 25 time intervals
+ * 
+ * 
+ * */
+function simpleAverage(list) {
+
+    let tot = 0;
+
+    list.forEach(element => {
+        tot += element;
+    });
+
+    return Math.floor(tot/list.length);
+}
+
+// NB Weighted average non cambia il risultato perché il valore block inserito è costante
+//  viene valuata solo la latenza di calcolo
+module.exports = async () => {
+
+    try {
+
+        console.log("init");
+
+        const rsf = await Framework.deployed();
+        const registryAddress = await rsf.computerRegistry();
+        const registry = await ComputerRegistry.at(registryAddress);
+        const address = await web3.currentProvider.getAddress();
+
+        let results = {};
+        let output = "'test.json'";
+
+        // Iterate over provided computers
+        for(pcId in computerObject) {
+
+            // Get computer
+            const pc = await registry.getComputer(parseInt(pcId));
+            const computer = await SimpleComputer.at(pc);
+
+            // Create entry in json result object
+            const computerName = config["ropsten"]["ratingComputer"][pcId];
+
+            results[""+computerName] = {};
+            console.log("Computer: " + computerName);
+
+            // Iterate over network degree
+            for(d in degrees) {
+
+                const degree = degrees[d];
+
+                results[computerName][degree] = {}; 
+
+                console.log("\tDegree: " + degree);
+                // Iterate over array of rating sizes
+                for(let j=0; j<elements.length; j++) {
+
+                    console.log("\t\tFilling with "+elements[j]+" scores");
+
+                    // Create input arrays
+                    let scores = []
+                    let blocks = []
+                    let addresses = []
+                
+                    for(let i=0; i<elements[j]; i++) {
+                        scores.push(Math.floor(Math.random()*10 + 1));
+                        blocks.push(10000);
+                        addresses.push(address);
+                    }
+
+                    let init;
+                    let end;
+                    let timeList = [];
+                        
+                    console.log("\t\tstart computing");
+
+                    // Iterate over the samples to take measurements
+                    for (let i=0; i<samples; i++) {
+                            
+                        let promises = [];
+
+                        init = new Date();
+
+                        // Create "degree" calls to compute()
+                        for(var k=0; k<degree; k++) {
+                            promises.push(computer.compute(scores, blocks, addresses));
+                        }
+                        
+                        // Wait for all these calls
+                        const list = await Promise.all(promises);
+
+                        // let res =  await computer.compute(scores, blocks, addresses);
+                        end = new Date();
+                        
+                        const elapsed = end.getTime() - init.getTime();
+                        console.log("\t\t* Time elapsed: " + elapsed);
+                        timeList.push(elapsed);
+                    }
+
+                    // Create entry in Result json
+                    results[computerName][degree][""+elements[j]] = timeList;
+                }
+            }
+        }
+
+        const json = JSON.stringify(results, null, 4);
+        fs.writeFileSync('./interactions/ropsten/Results/'+output, json, 'utf8'); // I don't know why readsync is ok and this one is not            
+    }
+    catch(e) {
+        console.log(e);
+    }
+
+    console.log("end");
+}
